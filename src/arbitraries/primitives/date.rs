312c3f9f67195ab0a3257132581ff80a171ed5e5
karl robeck alferez <karlalferezfx@gmail.com>
2026-04-14 10:24:25 +0800

    feat: add DateArbitrary struct and implement generate function for date generation

---
+++ src/arbitraries/primitives/date.rs
@@ -0,0 +1,43 @@
+use napi::JsDate;
+use napi_derive::napi;
+
+use crate::traits::Arbitrary;
+
+#[napi(object)]
+pub struct DateOption<'a> {
+    pub min: Option<JsDate<'a>>,
+    pub max: Option<JsDate<'a>>,
+    pub no_invalid_date: Option<bool>,
+}
+
+#[napi]
+pub struct DateArbitrary<'a>(pub(crate) DateOption<'a>);
+
+impl<'a> Arbitrary for DateArbitrary<'a> {
+    type Output = std::time::SystemTime;
+
+    fn generate(&self) -> Self::Output {
+        let option = &self.0;
+
+        let min = option
+            .min
+            .as_ref()
+            .map(|d| d.value_of().unwrap_or(0.0) as u64)
+            .unwrap_or(0);
+
+        let max = option
+            .max
+            .as_ref()
+            .map(|d| d.value_of().unwrap_or(0.0) as u64)
+            .unwrap_or(u64::MAX);
+
+        let no_invalid_date = option.no_invalid_date.unwrap_or(false);
+
+        if no_invalid_date && min > max {
+            return std::time::UNIX_EPOCH + std::time::Duration::from_secs(min);
+        }
+
+        let timestamp = rand::random::<u64>() % (max - min + 1) + min;
+        std::time::UNIX_EPOCH + std::time::Duration::from_secs(timestamp)
+    }
+}
